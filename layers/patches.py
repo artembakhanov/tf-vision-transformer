4843c0cf5c618969fc4dc6e956402d3bacae9197
Artem Bakhanov <artembakhanov@gmail.com>
2021-04-24 16:56:50 +0300

    Refactor patches layer

--- layers/patches.py
+++ layers/patches.py
@@ -9,16 +9,21 @@ class EmbeddedPatches(tf.keras.layers.Layer):
     This layers extracts patches of size P and 
     projects them to a latent space of size D;
     Add class and position embeddings.
+    
+    Args:
+        patch_size (int): The size of one side of a square patch.
+        latent_dim (int): The size of latent vectors in encoder layers.
+            All the patches will be projected to this dimension.
     """
     
-    def __init__(self, P, D):
+    def __init__(self, patch_size, latent_dim):
         super().__init__()
-        self.P = P
-        self.D = D
-        self.projection = Dense(self.D, use_bias=False)
+        self.patch_size = patch_size
+        self.latent_dim = latent_dim
+        self.projection = Dense(self.latent_dim, use_bias=False)
         self.class_emb = self.add_weight(
             name="class_emb",
-            shape=(1, 1, self.D),
+            shape=(1, 1, self.latent_dim),
             initializer="zeros"
         )
         self.broadcast_class_emb = None
@@ -32,12 +37,12 @@ class EmbeddedPatches(tf.keras.layers.Layer):
            
         
         (_, H, W, C) = input_shape
-        self.N = int(H * W / (self.P ** 2))
+        self.N = int(H * W / (self.patch_size ** 2))
         
-        if H * W % self.P ** 2 != 0:
+        if H * W % self.patch_size ** 2 != 0:
             warnings.warn(
                 f"It is recommended to have valid padding.\n"
-                f"Only {self.N * self.P}x{self.N * self.P} pixels will be considered.\n" 
+                f"Only {self.N * self.patch_size}x{self.N * self.patch_size} pixels will be considered.\n" 
                 f"Try to put another value of P if you are afraid of this warning."
             )
            
@@ -46,7 +51,7 @@ class EmbeddedPatches(tf.keras.layers.Layer):
         
         self.pos_emb = self.add_weight(
             name="pos_emb",
-            shape=(1, self.N + 1, self.D),
+            shape=(1, self.N + 1, self.latent_dim),
             initializer=RandomNormal(stddev=0.02) # FROM BERT
         )
     
@@ -54,24 +59,24 @@ class EmbeddedPatches(tf.keras.layers.Layer):
     def call(self, input):
         batch_size = tf.shape(input)[0]
         patches = tf.image.extract_patches(input, 
-                                        sizes=[1, self.P, self.P, 1], 
-                                        strides=[1, self.P, self.P, 1], 
+                                        sizes=[1, self.patch_size, self.patch_size, 1], 
+                                        strides=[1, self.patch_size, self.patch_size, 1], 
                                         rates=[1, 1, 1, 1], 
                                         padding="VALID")
         
         patches = self.projection(patches)
         
         # remove one dimension
-        patches = tf.reshape(patches, (batch_size, -1, self.D))
+        patches = tf.reshape(patches, (batch_size, -1, self.latent_dim))
         
         # add embedding
         bc_class_emb = tf.broadcast_to(self.class_emb, 
-                                            [batch_size, 1, self.D])
+                                            [batch_size, 1, self.latent_dim])
         patches = tf.concat((bc_class_emb, patches), axis=1)
         
         # add position embeddings
         bc_pos_emb = tf.broadcast_to(self.pos_emb,
-                                          [batch_size, self.N + 1, self.D])
+                                          [batch_size, self.N + 1, self.latent_dim])
         patches = patches + bc_pos_emb
         
         return patches
