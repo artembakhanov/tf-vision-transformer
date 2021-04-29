8916a0139a638d430695aad97fbcf8d4eb0319e2
artembakhanov <artembakhanov@gmail.com>
2021-04-29 03:58:07 +0300

    Create const.py

---
+++ const.py
@@ -0,0 +1,9 @@
+DECAY_TYPES = ["linear", "cosine"]
+DATASETS = ["fruits-360", "mnist"]
+CLASSES_NUM = {
+    # version 2020.05.18.0
+    "fruits-360": 131,
+    
+    # digit mnist
+    "mnist": 10
+}
